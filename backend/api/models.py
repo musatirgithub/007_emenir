d1012071cd1257ba71f53503831d4026f0d0f2e8
Mehmet Ugur SATIR <musatir@gmail.com>
2023-05-13 09:15:17 +0300

    Expert & Sigorta Models created

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Expert(models.Model):
+    isim = models.CharField(max_length=25)
+    soyisim = models.CharField(max_length=25)
+    iban = models.BigIntegerField()
+    telefon = models.BigIntegerField()
+    email = models.CharField(max_length=25, unique=True)
+
+
+class Sigorta(models.Model):
+    firma_adi = models.CharField(max_length=25)
+    adres = models.CharField(max_length=100)
+    telefon = models.BigIntegerField()
+    email = models.CharField(max_length=25, unique=True)
