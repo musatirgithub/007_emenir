e0d24a456485d6ddb3823f783d9a71b952a7c142
Mehmet Ugur SATIR <musatir@gmail.com>
2023-05-14 23:24:44 +0300

    Araci model is added

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Expert(models.Model):
     isim = models.CharField(max_length=25)
     soyisim = models.CharField(max_length=25)
-    iban = models.BigIntegerField()
+    iban = models.CharField(max_length=26, blank=True, null=True)
     telefon = models.BigIntegerField()
     email = models.CharField(max_length=25, unique=True)
 
@@ -14,5 +14,15 @@ class Expert(models.Model):
 class Sigorta(models.Model):
     firma_adi = models.CharField(max_length=25)
     adres = models.CharField(max_length=100)
+    telefon = models.BigIntegerField()
+    email = models.CharField(max_length=25, unique=True)
+
+
+class Araci(models.Model):
+    calistigi_sirket = models.CharField(max_length=50)
+    kimlik_no = models.CharField(max_length=11, blank=True, null=True)
+    isim = models.CharField(max_length=25)
+    soyisim = models.CharField(max_length=25)
+    iban = models.CharField(max_length=26, blank=True, null=True)
     telefon = models.BigIntegerField()
     email = models.CharField(max_length=25, unique=True)
