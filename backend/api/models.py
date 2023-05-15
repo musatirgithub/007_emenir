6921845190084df53dd7c336265029b39b328873
Mehmet Ugur SATIR <musatir@gmail.com>
2023-05-15 15:22:53 +0300

    arac, anlasma_modeli, kaza_kaydı models are added

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import Profile
 
 # Create your models here.
 
@@ -26,3 +27,39 @@ class Araci(models.Model):
     iban = models.CharField(max_length=26, blank=True, null=True)
     telefon = models.BigIntegerField()
     email = models.CharField(max_length=25, unique=True)
+
+
+class Arac(models.Model):
+    plaka = models.CharField(max_length=11)
+    sigorta = models.CharField(max_length=50)
+    sigorta_no = models.IntegerField()
+
+
+class AnlasmaModeli(models.Model):
+    model_adi = models.CharField(max_length=50)
+
+
+class KazaKaydi(models.Model):
+    ASAMA = (
+        ("1", "Sigorta Şirketinden Değer Kaybı Talebi "),
+        ("2", "Tahkim Komisyonunda Ön İnceleme"),
+        ("3", "Tahkim Komisyon Heyetince İnceleme"),
+        ("4", "Tutarın Sigorta Şirketince Ödenmesi"),
+        ("5", "Süreç Tamamlandı")
+    )
+    arac = models.ForeignKey(
+        Arac, on_delete=models.SET_NULL, blank=True, null=True)
+    araci = models.ForeignKey(
+        Araci, on_delete=models.SET_NULL, blank=True, null=True)
+    profil = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, blank=True, null=True)
+    sigorta = models.ForeignKey(
+        Sigorta, on_delete=models.SET_NULL, blank=True, null=True)
+    anlasma_modeli = models.ForeignKey(
+        AnlasmaModeli, on_delete=models.SET_NULL, blank=True, null=True)
+    asama = models.PositiveSmallIntegerField(max_length=1, choices=ASAMA)
+    asama_tarihi = models.DateField()
+    profil = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, blank=True, null=True)
+    expert = models.ForeignKey(
+        Expert, on_delete=models.SET_NULL, blank=True, null=True)
