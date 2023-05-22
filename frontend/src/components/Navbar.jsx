33256a5766f6fc5650f3a0001843c04075d269b5
Mehmet Ugur SATIR <musatir@gmail.com>
2023-05-22 23:10:12 +0300

    navbar added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { currentUser } = useSelector((state) => state.auth);
   return (
-    <div>
-      <ul>
+    <div className="flex justify-between">
+      <div className="flex gap-[1rem]">
+        <img
+          src="https://i.cbc.ca/1.4262961.1503689323!/fileImage/httpImage/lady-justice.jpg"
+          alt="justice"
+          class="object-cover h-24 w-48"
+        />
+        <p>Emenir Hukuk</p>
+        {currentUser ? <p>Merhaba ${currentUser}</p> : <p>Merhaba ziyaretçi</p>}
+      </div>
+      <ul className="hidden lg:block lg:flex gap-[1rem]">
         <li>Home</li>
         <li>About</li>
         <li>Değer Kaybı</li>
         <li>İletişim</li>
-        <li>Giriş</li>
+
+        <li>
+          <Link to="/login">Giriş</Link>
+        </li>
         <li>Kayıt</li>
       </ul>
     </div>
