fcfb331cfc89cd2001ff7125fca5a3d0bd32eec7
Mehmet Ugur Satir <musatir@gmail.com>
2023-07-24 22:41:04 +0200

    phaseConvertor Turkish character fix

--- frontend/src/helper/phaseConverter.js
+++ frontend/src/helper/phaseConverter.js
@@ -3,7 +3,7 @@ const phases = {
   2: "Tahkim Komisyonunda Ön İnceleme",
   3: "Tahkim Komisyon Heyetince İnceleme",
   4: "Tutarın Sigorta Şirketince Ödenmesi",
-  5: "Sürec Tamamlandi",
+  5: "Süreç Tamamlandı",
 };
 export const phaseConverter = (phase) => {
   return phases[phase];
