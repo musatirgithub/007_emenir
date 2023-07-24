ab2cb3d3b1f40d655cf43f36628de6a116e2ba23
Mehmet Ugur Satir <musatir@gmail.com>
2023-07-24 22:23:43 +0200

    classNames updated for span element in phases comp

--- frontend/src/components/Phase.jsx
+++ frontend/src/components/Phase.jsx
@@ -35,16 +35,16 @@ const Phase = ({ asama }) => {
         <div
           className={`${
             asama >= 2
-              ? "w-[1.5rem] h-[1.5rem] leading-[1.5rem] bg-[#C19461] rounded-full text-center relative lg:w-[3rem] lg:h-[3rem] lg:leading-[3rem]"
-              : "w-[1.5rem] h-[1.5rem] leading-[1.5rem] bg-[#000]/[0.3] rounded-full text-center lg:w-[3rem] lg:h-[3rem] lg:leading-[3rem]"
+              ? "w-[1.5rem] h-[1.5rem] leading-[1.5rem] bg-[#C19461] rounded-full text-center relative lg:w-[3rem] lg:h-[3rem] lg:leading-[3rem] group"
+              : "w-[1.5rem] h-[1.5rem] leading-[1.5rem] bg-[#000]/[0.3] rounded-full text-center lg:w-[3rem] lg:h-[3rem] lg:leading-[3rem] group"
           }`}
         >
           2
           <span
             className={`${
               asama == 2
-                ? "absolute top-[2rem] left-[1rem] text-[1rem] w-[10rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
-                : "hidden"
+              ? "absolute top-[2rem] left-[2rem] p-[0.2rem] text-[1rem] w-[10rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
+              : "hidden p-[0.2rem] group-hover:absolute group-hover:top-[2rem] group-hover:left-[2rem] group-hover:text-[1rem] group-hover:z-10 group-hover:bg-[#FFF] group-hover:bg-opacity-100  group-hover:w-[10rem] group-hover:translate-x-[-50%] group-hover:leading-[1.2rem] group-hover:block group-hover:lg:top-[3rem] group-hover:lg:left-[1rem]"
             }`}
           >
             {phaseConverter(2)}
@@ -70,8 +70,8 @@ const Phase = ({ asama }) => {
           <span
             className={`${
               asama == 3
-                ? "absolute top-[2rem] left-[1rem] text-[1rem] w-[10rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
-                : "hidden"
+              ? "absolute top-[2rem] left-[2rem] p-[0.2rem] text-[1rem] w-[10rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
+              : "hidden p-[0.2rem] group-hover:absolute group-hover:top-[2rem] group-hover:left-[2rem] group-hover:text-[1rem] group-hover:z-10 group-hover:bg-[#FFF] group-hover:bg-opacity-100  group-hover:w-[10rem] group-hover:translate-x-[-50%] group-hover:leading-[1.2rem] group-hover:block group-hover:lg:top-[3rem] group-hover:lg:left-[1rem]"
             }`}
           >
             {phaseConverter(3)}
@@ -97,8 +97,8 @@ const Phase = ({ asama }) => {
           <span
             className={`${
               asama == 4
-                ? "absolute top-[2rem] left-[1rem] text-[1rem] w-[10rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
-                : "hidden"
+              ? "absolute top-[2rem] left-[2rem] p-[0.2rem] text-[1rem] w-[10rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
+              : "hidden p-[0.2rem] group-hover:absolute group-hover:top-[2rem] group-hover:left-[2rem] group-hover:text-[1rem] group-hover:z-10 group-hover:bg-[#FFF] group-hover:bg-opacity-100  group-hover:w-[10rem] group-hover:translate-x-[-50%] group-hover:leading-[1.2rem] group-hover:block group-hover:lg:top-[3rem] group-hover:lg:left-[1rem]"
             }`}
           >
             {phaseConverter(4)}
@@ -124,8 +124,8 @@ const Phase = ({ asama }) => {
           <span
             className={`${
               asama == 5
-                ? "absolute top-[2rem] left-[0.8rem] text-[1rem] w-[8rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
-                : "hidden"
+              ? "absolute top-[2rem] left-[2rem] p-[0.2rem] text-[1rem] w-[10rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
+              : "hidden p-[0.2rem] group-hover:absolute group-hover:top-[2rem] group-hover:left-[2rem] group-hover:text-[1rem] group-hover:z-10 group-hover:bg-[#FFF] group-hover:bg-opacity-100  group-hover:w-[10rem] group-hover:translate-x-[-50%] group-hover:leading-[1.2rem] group-hover:block group-hover:lg:top-[3rem] group-hover:lg:left-[1rem]"
             }`}
           >
             {phaseConverter(5)}
