32869e8d57ea511ad92f59ae963183a24370e430
Mehmet Ugur Satir <musatir@gmail.com>
2023-07-24 22:20:35 +0200

    phase hover provides phase name

--- frontend/src/components/Phase.jsx
+++ frontend/src/components/Phase.jsx
@@ -8,19 +8,19 @@ const Phase = ({ asama }) => {
         <div
           className={`${
             asama >= 1
-              ? "w-[1.5rem] h-[1.5rem] leading-[1.5rem] bg-[#C19461] rounded-full text-center relative lg:w-[3rem] lg:h-[3rem] lg:leading-[3rem]"
-              : "w-[1.5rem] h-[1.5rem] leading-[1.5rem] bg-[#000]/[0.3] rounded-full text-center lg:w-[3rem] lg:h-[3rem] lg:leading-[3rem]"
+              ? "w-[1.5rem] h-[1.5rem] leading-[1.5rem] bg-[#C19461] rounded-full text-center relative lg:w-[3rem] lg:h-[3rem] lg:leading-[3rem] group"
+              : "w-[1.5rem] h-[1.5rem] leading-[1.5rem] bg-[#000]/[0.3] rounded-full text-center lg:w-[3rem] lg:h-[3rem] lg:leading-[3rem] group"
           }`}
         >
           1
           <span
             className={`${
               asama == 1
-                ? "absolute top-[2rem] left-[2rem] text-[1rem] w-[10rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
-                : "hidden"
+                ? "absolute top-[2rem] left-[2rem] p-[0.2rem] text-[1rem] w-[10rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
+                : "hidden p-[0.2rem] group-hover:absolute group-hover:top-[2rem] group-hover:left-[2rem] group-hover:text-[1rem] group-hover:z-10 group-hover:bg-[#FFF] group-hover:bg-opacity-100  group-hover:w-[10rem] group-hover:translate-x-[-50%] group-hover:leading-[1.2rem] group-hover:block group-hover:lg:top-[3rem] group-hover:lg:left-[1rem]"
             }`}
           >
-            {phaseConverter(asama)}
+            {phaseConverter(1)}
           </span>
         </div>
       </div>
@@ -47,7 +47,7 @@ const Phase = ({ asama }) => {
                 : "hidden"
             }`}
           >
-            {phaseConverter(asama)}
+            {phaseConverter(2)}
           </span>
         </div>
       </div>
@@ -74,7 +74,7 @@ const Phase = ({ asama }) => {
                 : "hidden"
             }`}
           >
-            {phaseConverter(asama)}
+            {phaseConverter(3)}
           </span>
         </div>
       </div>
@@ -101,7 +101,7 @@ const Phase = ({ asama }) => {
                 : "hidden"
             }`}
           >
-            {phaseConverter(asama)}
+            {phaseConverter(4)}
           </span>
         </div>
       </div>
@@ -128,7 +128,7 @@ const Phase = ({ asama }) => {
                 : "hidden"
             }`}
           >
-            {phaseConverter(asama)}
+            {phaseConverter(5)}
           </span>
         </div>
       </div>
