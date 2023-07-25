038964741b73f7ecc8b8bda9a92a2ef4dd624181
Mehmet Ugur Satir <musatir@gmail.com>
2023-07-25 13:40:37 +0200

    phases small screen phase-5 out of screen fix

--- frontend/src/components/Phase.jsx
+++ frontend/src/components/Phase.jsx
@@ -128,8 +128,8 @@ const Phase = ({ asama }) => {
           <span
             className={`${
               asama == 5
-              ? "absolute top-[2rem] left-[2rem] p-[0.2rem] text-[1rem] w-[10rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
-              : "hidden p-[0.2rem] group-hover:absolute group-hover:top-[2rem] group-hover:left-[2rem] group-hover:text-[1rem] group-hover:z-10 group-hover:bg-[#F2F2F2] group-hover:bg-opacity-100  group-hover:w-[10rem] group-hover:translate-x-[-50%] group-hover:leading-[1.2rem] group-hover:block group-hover:lg:top-[3rem] group-hover:lg:left-[1rem]"
+              ? "absolute top-[2rem] left-[2rem] p-[0.2rem] text-[1rem] w-[8rem] translate-x-[-50%] leading-[1.2rem] lg:top-[3rem] lg:left-[1rem]"
+              : "hidden p-[0.2rem] group-hover:absolute group-hover:top-[2rem] group-hover:left-[2rem] group-hover:text-[1rem] group-hover:w-[8rem] group-hover:z-10 group-hover:bg-[#F2F2F2] group-hover:bg-opacity-100  group-hover:w-[10rem] group-hover:translate-x-[-50%] group-hover:leading-[1.2rem] group-hover:block group-hover:lg:top-[3rem] group-hover:lg:left-[1rem]"
             }`}
           >
             {phaseConverter(5)}
